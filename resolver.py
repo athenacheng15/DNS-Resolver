26c30768c2d9b340b4aa7e8df00ba064c20a99c5
Athena Cheng <athenachengyc15@gmail.com>
2026-07-12 16:27:09 +1000

    feat(resolver): decode client queries

--- resolver.py
+++ resolver.py
@@ -1,7 +1,9 @@
-import socket
-import sys
+from asyncio import queues
+import sys, socket
 
 from root_hints import parse_root_hints
+from dns_message import parse_header, parse_questions
+from dns_records import TYPE_MAP, CLASS_MAP
 
 
 def parse_args():
@@ -43,3 +45,39 @@ def run_server(server_socket):
     while True:
         query_data, client_address = server_socket.recvfrom(MAX_DNS_MESSAGE_SIZE)
         print(f"Received {len(query_data)} bytes from {client_address}")
+
+
+def decoode_client_query(query_data):
+    header, offset = parse_header(query_data)
+    questions, offset = parse_questions(query_data, offset, header.qdcount)
+
+    return header, questions
+
+
+def create_request_state(client_address, header, questions):
+    client_ip, client_port = client_address
+
+    return {
+        "client_address": client_ip,
+        "client_port": client_port,
+        "original_id": header.message_id,
+        "questions": questions,
+    }
+
+
+def print_request_state(request_state):
+    question = request_state["question"]
+
+    qtype_name = TYPE_MAP.get(question.qtype, f"UNKNOWN {question.qtype}")
+    qclass_name = CLASS_MAP.get(question.qclass, f"UNKNOWN {question.qclass}")
+
+    print("--- CLIENT QUERY ---")
+    print(
+        f"Client: "
+        f"{request_state['client_address']}:"
+        f"{request_state['client_port']}"
+    )
+    print(f"ID: {request_state['original_id']}")
+    print(f"QNAME: {question.qname}")
+    print(f"QTYPE: {question.qtype} ({qtype_name})")
+    print(f"QCLASS: {question.qclass} ({qclass_name})")
