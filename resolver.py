a426f1428c5af050ffb3bb0a6c14cde9bb890b63
Athena Cheng <athenachengyc15@gmail.com>
2026-07-12 16:08:32 +1000

    feat(resolver): load root hints

--- resolver.py
+++ resolver.py
@@ -1,5 +1,7 @@
-from socket import timeout
+import socket
 import sys
+
+from root_hints import parse_root_hints
 
 
 def parse_args():
@@ -12,3 +14,12 @@ def parse_args():
     listen_port = int(sys.argv[3])
 
     return root_hints_file, timeout, listen_port
+
+
+def load_root_hints(root_hints_file):
+    root_ns_records, root_a_records, root_a_map = parse_root_hints(root_hints_file)
+
+    print(f"Loaded {len(root_ns_records)} root NS records")
+    print(f"Loaded {len(root_a_records)} root A records")
+
+    return root_ns_records, root_a_records, root_a_map
