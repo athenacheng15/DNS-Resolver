bcdf01c216122daffe7f5eb1e8b8d7bc4f88c1aa
Athena Cheng <athenachengyc15@gmail.com>
2026-07-12 17:31:49 +1000

    feat(encoder): encode DNS response header fields

--- dns_encoder.py
+++ dns_encoder.py
@@ -40,3 +40,37 @@ def encode_question(question):
         + encode_uint16(question.qtype)
         + encode_uint16(question.qclass)
     )
+
+
+def encode_header(
+    message_id, flags, question_count, answer_count, authority_count, additional_count
+):
+    return struct.pack(
+        "!HHHHHH",
+        message_id,
+        flags,
+        question_count,
+        answer_count,
+        authority_count,
+        additional_count,
+    )
+
+
+def encode_a_rdata(address):
+    try:
+        # inet_aton Convert IPv4 address string into 4 network bytes. (e.g. b'\xc6)\x00\x04')
+        return socket.inet_aton(address)
+    except OSError as e:
+        raise ValueError(f"Invalid IPv4 address: {address}") from e
+
+
+def build_response_flags(query_header, rcode=0):
+    flags = 0
+
+    flags |= 1 << 15  # | for bitwise OR
+    flags |= (query_header.opcode & 0xF) << 11
+    flags |= (query_header.rd & 0x1) << 8
+    flags |= 1 << 7
+    flags |= rcode & 0xF
+
+    return flags
