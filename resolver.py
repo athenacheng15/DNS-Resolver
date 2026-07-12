bde31dd2a073969d26b58c67f06a3dc0fde24df6
Athena Cheng <athenachengyc15@gmail.com>
2026-07-12 16:16:06 +1000

    feat(resolver): add UDP server socket

--- resolver.py
+++ resolver.py
@@ -23,3 +23,23 @@ def load_root_hints(root_hints_file):
     print(f"Loaded {len(root_a_records)} root A records")
 
     return root_ns_records, root_a_records, root_a_map
+
+
+def create_server_socket(listen_port):
+    # socket.AF_INET for IPv4, socket.SOCK_DGRAM for UDP
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_address = ("127.0.0.1", listen_port)
+    server_socket.bind(server_address)
+
+    print(f"Resolver server socket  listening on 127.0.0.1:{listen_port}")
+
+    return server_socket
+
+
+MAX_DNS_MESSAGE_SIZE = 4096
+
+
+def run_server(server_socket):
+    while True:
+        query_data, client_address = server_socket.recvfrom(MAX_DNS_MESSAGE_SIZE)
+        print(f"Received {len(query_data)} bytes from {client_address}")
